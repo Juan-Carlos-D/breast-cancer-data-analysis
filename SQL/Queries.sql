f258c89f436862889b156f253f3863d17e1b3352
Juan Carlos Dominguez <jcarlosdominguez38@gmail.com>
2025-01-16 18:43:25 -0800

    inner joins

--- SQL/Queries.sql
+++ SQL/Queries.sql
@@ -33,3 +33,34 @@ VALUES
 (572, 14.00, 18.50, 90.0, 600.0, 0.1150, 0.2100, 0.2800, 0.1100, 0.2400, 0.0750);
 
 SELECT * FROM mean_attributes WHERE case_id > 569;
+
+# -- Inner Join 
+SELECT cc.diagnosis, 
+	AVG(mean_attributes.radius_mean) AS avg_radius, 
+	AVG(mean_attributes.texture_mean) AS avg_texture
+FROM cancer_cases cc
+INNER JOIN mean_attributes ON cc.case_id = mean_attributes.case_id
+GROUP BY cc.diagnosis;
+
+# -- Left Join
+SELECT cancer_cases.case_id, cancer_cases.diagnosis, worst_attributes.texture_worst, worst_attributes.perimeter_worst
+FROM cancer_cases
+LEFT JOIN worst_attributes ON cancer_cases.case_id = worst_attributes.case_id;
+
+# -- Inner Join
+SELECT cancer_cases.case_id, cancer_cases.diagnosis, mean_attributes.radius_mean, worst_attributes.texture_worst
+FROM cancer_cases
+INNER JOIN mean_attributes ON cancer_cases.case_id = mean_attributes.case_id
+INNER JOIN worst_attributes ON cancer_cases.case_id = worst_attributes.case_id;
+
+# -- Inner Join w. Multiple Conditions
+SELECT cancer_cases.case_id, cancer_cases.diagnosis, mean_attributes.radius_mean, worst_attributes.texture_worst
+FROM cancer_cases
+INNER JOIN mean_attributes ON cancer_cases.case_id = mean_attributes.case_id
+INNER JOIN worst_attributes ON cancer_cases.case_id = worst_attributes.case_id
+WHERE mean_attributes.radius_mean > 15 AND worst_attributes.texture_worst < 25;
+
+# -- Self Inner Join
+SELECT a.case_id AS case_id_1, b.case_id AS case_id_2, a.radius_mean, b.radius_mean
+FROM mean_attributes a
+INNER JOIN mean_attributes b ON a.radius_mean > b.radius_mean;
